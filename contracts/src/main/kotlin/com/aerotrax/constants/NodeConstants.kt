22fc205f92007cb052bf943ad11843470e121034
christian.dismaya <christiand@quantumcrowd.io>
2020-02-03 13:48:50 +0800

    aerotrax

--- contracts/src/main/kotlin/com/aerotrax/constants/NodeConstants.kt
+++ contracts/src/main/kotlin/com/aerotrax/constants/NodeConstants.kt
@@ -5,14 +5,16 @@ class NodeConstants{
     companion object{
 
          const val AEROTRAX_NODE = "O=Aerotrax,L=London,C=GB"
+         const val MRO_NODE = "O=MRO,L=London,C=GB"
+         const val OEM_NODE = "O=OEM,L=London,C=GB"
+         const val AIRLINE1_NODE = "O=Airline1,L=London,C=GB"
+         const val AIRLINE2_NODE = "O=Airline2,L=London,C=GB"
 
-         const val MRO_NODE = "O=MRO,L=New York,C=US"
-
-         const val OEM_NODE = "O=OEM,L=New York,C=US"
-
-         const val AIRLINE1_NODE = "O=Airline1,L=New York,C=US"
-
-         const val AIRLINE2_NODE = "O=Airline2,L=New York,C=US"
+//        const val AEROTRAX_NODE = "O=qwe,L=London,C=GB"
+//        const val MRO_NODE = "O=wer,L=London,C=GB"
+//        const val OEM_NODE = "O=sdf,L=London,C=GB"
+//        const val AIRLINE1_NODE = "O=uio,L=London,C=GB"
+//        const val AIRLINE2_NODE = "O=ert,L=London,C=GB"
 
     }
 }
